c92c52a18b927d99c7dc54a54be3de870dfe366a
Petr Pridal <petr.pridal@klokantech.com>
2016-01-07 18:18:50 +0100

    Changing main to index in resources.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function (launchData) {
-  chrome.app.window.create('main.html', {
+  chrome.app.window.create('index.html', {
     'id':'main',
     'bounds': {
         'width': Math.round(window.screen.availWidth*0.8),
